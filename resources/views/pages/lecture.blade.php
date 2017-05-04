04b1e5d5149ca3de35d9b6514c909f94d692657c
Jeewoo Yoon <yoonjeewoo@gmail.com>
2017-05-05 00:00:56 +0900

    scroll error solved

--- resources/views/pages/lecture.blade.php
+++ resources/views/pages/lecture.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div id="page-content-wrapper">
+<div id="page-content-wrapper" style="margin-top: 50px;">
     <div class="container-fluid">
         <div class="row">
             <div class="col-lg-12">
