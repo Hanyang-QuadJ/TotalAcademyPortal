6e19d726d497a1e3f9a6713e2fd94a05505fc5b5
Yoon Jee Woo <yoonjeewoo@gmail.com>
2017-05-06 19:08:06 +0900

    [v0.0.1] 대시보드 수정

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,54 +34,54 @@
     </script>
 </head>
 <body>
-    <div id="app">
-      <nav class="navbar navbar-default navbar-fixed-top">
-          <div class="container">
-              <div class="navbar-header">
-                  <a href="#menu-toggle" class="navbar-brand" id="menu-toggle">
-                    <img src="{{ asset('image/logo.png') }}" alt="Total Academy Portal" height="50px;" width="auto;">
-                  </a>
-              </div>
+    <header class="header">
+      <div id="app">
+        <nav class="navbar navbar-default navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <a href="#menu-toggle" class="navbar-brand" id="menu-toggle">
+                      <img src="{{ asset('image/logo.png') }}" alt="Total Academy Portal" height="50px;" width="auto;">
+                    </a>
+                </div>
+                <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="nav navbar-nav">
+                        &nbsp;
+                    </ul>
 
-              <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                  <!-- Left Side Of Navbar -->
-                  <ul class="nav navbar-nav">
-                      &nbsp;
-                  </ul>
+                    <!-- Right Side Of Navbar -->
+                    <ul class="nav navbar-nav navbar-right">
+                        <!-- Authentication Links -->
+                        @if (Auth::guest())
 
-                  <!-- Right Side Of Navbar -->
-                  <ul class="nav navbar-nav navbar-right">
-                      <!-- Authentication Links -->
-                      @if (Auth::guest())
+                            <li><a href="{{ route('login') }}">Login</a></li>
+                            <li><a href="{{ route('register') }}">Register</a></li>
+                        @else
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+                                <ul class="dropdown-menu" role="menu">
+                                    <li>
+                                        <a href="{{ route('logout') }}"
+                                            onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                            Logout
+                                        </a>
 
-                          <li><a href="{{ route('login') }}">Login</a></li>
-                          <li><a href="{{ route('register') }}">Register</a></li>
-                      @else
-                          <li class="dropdown">
-                              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                  {{ Auth::user()->name }} <span class="caret"></span>
-                              </a>
-
-                              <ul class="dropdown-menu" role="menu">
-
-                                  <li>
-                                      <a href="{{ route('logout') }}"
-                                          onclick="event.preventDefault();
-                                                   document.getElementById('logout-form').submit();">
-                                          Logout
-                                      </a>
-
-                                      <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                          {{ csrf_field() }}
-                                      </form>
-                                  </li>
-                              </ul>
-                          </li>
-                      @endif
-                  </ul>
-              </div>
-          </div>
-      </nav>
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                            {{ csrf_field() }}
+                                        </form>
+                                    </li>
+                                </ul>
+                            </li>
+                        @endif
+                    </ul>
+                </div>
+            </div>
+        </nav>
+      </header>
+      <section class="main">
         @if (Auth::check())
           <div id="wrapper" class="toggled">
             @include('layouts.sidebar')
@@ -93,6 +93,7 @@
             @yield('content')
           </div>
         @endif
+      </section>
     </div>
 
     <!-- Scripts -->
--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -2,79 +2,10 @@
 
 @section('content')
   <div id="page-content-wrapper" style="margin-top: 50px;">
-      <div class="container-fluid table-parent">
-        <div class="table-child">
-        <table class="table table-hover">
-          <tr>
-            <th>hello</th>
-            <th>hello</th>
-            <th>hello</th>
-          </tr>
-          <tr>
-            <td>hello</td>
-            <td>hello</td>
-            <td>hello</td>
-          </tr>
-          <tr>
-            <td>hello</td>
-            <td>hello</td>
-            <td>hello</td>
-          </tr>
-          <tr>
-            <td>hello</td>
-            <td>hello</td>
-            <td>hello</td>
-          </tr>
-        </table>
+    <div class="progress">
+      <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width: 45%">
+        <span class="sr-only">45% Complete</span>
       </div>
-      <div class="table-child">
-        <table class="table table-hover">
-          <tr>
-            <th>hello</th>
-            <th>hello</th>
-            <th>hello</th>
-          </tr>
-          <tr>
-            <td>hello</td>
-            <td>hello</td>
-            <td>hello</td>
-          </tr>
-          <tr>
-            <td>hello</td>
-            <td>hello</td>
-            <td>hello</td>
-          </tr>
-          <tr>
-            <td>hello</td>
-            <td>hello</td>
-            <td>hello</td>
-          </tr>
-        </table>
-      </div>
-      <div class="table-child">
-        <table class="table table-hover">
-          <tr>
-            <th>hello</th>
-            <th>hello</th>
-            <th>hello</th>
-          </tr>
-          <tr>
-            <td>hello</td>
-            <td>hello</td>
-            <td>hello</td>
-          </tr>
-          <tr>
-            <td>hello</td>
-            <td>hello</td>
-            <td>hello</td>
-          </tr>
-          <tr>
-            <td>hello</td>
-            <td>hello</td>
-            <td>hello</td>
-          </tr>
-        </table>
-      </div>
-  </div>
+    </div>
 </div>
 @endsection
