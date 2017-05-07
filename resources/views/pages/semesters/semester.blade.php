3a4da0f07f5d2a28b8c86f8826a48ca9ec4dab93
KorJeongHyeonLee <nomar0107@gmail.com>
2017-05-07 18:03:24 +0900

    [v0.0.1] 하긱수정

--- resources/views/pages/semesters/semester.blade.php
+++ resources/views/pages/semesters/semester.blade.php
@@ -1,11 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-    <div id="page-content-wrapper" class="my_container">
+    <div id="page-content-wrapper" style="margin-top: 50px;">
         <div class="container-fluid">
+            <div class="submit-btn">
+                <a href="/semester/create">
+                    <button class="btn btn-default btn-block">학기등록</button>
+                </a>
+            </div>
             <div class="row">
                 <div class="col-lg-12">
-                    <h1>학기페이지입니다.</h1>
                     <table id="semester-table" class="table table-striped table-bordered" cellspacing="0">
                         <thead>
                         <tr>
@@ -33,27 +37,20 @@
                                 <td>
                                     <button class="btn btn-success">자세히</button>
                                     <a href="/semester/{{$semester->id}}/edit">
-                                        <button class="btn btn-warning">수정</button>
+                                        <button class="btn btn-default">수정</button>
                                     </a>
-                                    <form action="/semester/{{$semester->id}}" method="post">
+                                    <form class="form-inline" action="/semester/{{$semester->id}}" method="post">
                                         {{method_field('DELETE')}}
                                         {{ csrf_field() }}
-                                        <button class="btn btn-danger">삭제</button>
+                                        <button class="btn btn-danger">퇴원</button>
                                     </form>
                                 </td>
                             </tr>
                         @endforeach
                         </tbody>
                     </table>
-                    <div class="col-lg-12">
-                        <a href="/semester/create">
-                            <button class="btn btn-primary btn-block">학기등록</button>
-                        </a>
-                    </div>
-                    <hr>
                 </div>
             </div>
         </div>
     </div>
-
 @endsection
