af4ea9264356c38e508a12386e1c86df4da0852d
Yoon Jee Woo <yoonjeewoo@gmail.com>
2017-05-08 23:13:24 +0900

    강좌 자세히보기

--- resources/views/pages/courses/show.blade.php
+++ resources/views/pages/courses/show.blade.php
@@ -5,35 +5,40 @@
             <h2>{{$course->name}} 상세정보</h2>
             <h4>{{$course->semester->name}}</h4>
             <hr>
-            <h3>학생목록</h3>
-            @foreach($course->students->sortBy('name') as $student)
-                <div class="col-md-4">
-                    <div class="panel panel-success">
-
-                        <div class="panel-body">
-                            <a href="/student/{{$student->id}}">{{$student->name}}</a><br>
-                            <span>{{$student->pivot->fee}}원</span>
-                            <a href="/course/student/edit/{{$course->id}}/{{$student->id}}"><button class="btn btn-primary">수강료수정</button></a>
-                            <form class="form-inline"
-                                  action="/course/student/{{$course->id}}/{{$student->id}}" method="post">
-                                {{method_field('DELETE')}}
-                                {{ csrf_field() }}
-                                <button class="btn btn-danger">수강 취소</button>
-                            </form>
-
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h4>수강생 목록</h4>
                         </div>
-
+                        <!-- Table -->
+                        <table class="table">
+                            <tr>
+                                <th>학생</th>
+                                <th>수강료</th>
+                            </tr>
+                            @foreach($course->students->sortBy('name') as $student)
+                                <tr>
+                                    <td><a href="/student/{{$student->id}}">{{$student->name}}</a></td>
+                                    <td>{{$student->pivot->fee}}원</td>
+                                    <td>
+                                        <a href="/course/student/edit/{{$course->id}}/{{$student->id}}"><button class="btn btn-default">수강료수정</button></a>
+                                        <form class="form-inline"
+                                              action="/course/student/{{$course->id}}/{{$student->id}}" method="post">
+                                            {{method_field('DELETE')}}
+                                            {{ csrf_field() }}
+                                            <button class="btn btn-danger">수강 취소</button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </table>
+                        <div class="panel-footer pull-right">
+                            <a href="/course/student/create/{{$course->id}}">학생추가</a>
+                        </div>
                     </div>
                 </div>
-            @endforeach
-            <div class="col-md-12">
-
-                <a href="/course/student/create/{{$course->id}}"><button href="" class="btn btn-warning">학생추가</button></a>
-
             </div>
-
-
-
         </div>
     </div>
 @endsection
