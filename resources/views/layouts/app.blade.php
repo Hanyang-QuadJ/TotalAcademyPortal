afaa4fff4f571d2ea35f014f225a274436eb59ec
Yoon Jee Woo <yoonjeewoo@gmail.com>
2017-05-08 23:25:18 +0900

    차트 사이즈 수

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -59,8 +59,8 @@
 
         // Set chart options
         var options = {'title':'How Much Pizza I Ate Last Night',
-                       'width':400,
-                       'height':300};
+                       'width':800,
+                       'height':600};
 
         // Instantiate and draw our chart, passing in some options.
         var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
