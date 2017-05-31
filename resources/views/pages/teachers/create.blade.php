b59db3d400d2be24c36f72c2c956185e3f86b320
ShinHyunJong <kashiashin@gmail.com>
2017-05-31 09:32:33 +0900

    `2424

--- resources/views/pages/teachers/create.blade.php
+++ resources/views/pages/teachers/create.blade.php
@@ -16,12 +16,17 @@
                         </div>
                         <div class="input-group">
                             <div class="input-group-addon"><i class="fa fa-user"></i></div>
-                            <input type="text" name="teacherPhone" class="form-control" placeholder="번호를 입력하세요">
+                            <input type="text" name="teacherPhone" class="form-control" placeholder="휴대폰번호를 입력하세요">
                         </div>
                         <div class="input-group">
                             <div class="input-group-addon"><i class="fa fa-user"></i></div>
-                            <input type="text" name="address" class="form-control" placeholder="이름을 입력하세요">
+                            <input type="text" name="address" class="form-control" placeholder="주소를 입력하세요">
                         </div>
+                        <div class="input-group">
+                            <div class="input-group-addon"><i class="fa fa-user"></i></div>
+                            <input type="text" name="address" class="form-control" placeholder="생년월일을 입력하세요">
+                        </div>
+                        <textarea name="memo" class="form-control" placeholder="특이사항을 입력하세요" rows="5"></textarea>
                     </div>
                     <button type="submit" class="btn new-btn">등록</button>
                 </form>
--- resources/views/pages/teachers/teacher.blade.php
+++ resources/views/pages/teachers/teacher.blade.php
@@ -6,6 +6,38 @@
       <div class="submit-btn">
           <a href="/teacher/create"><button class="btn btn-default btn-block">강사등록</button></a>
       </div>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-4">
+                    <h1>강사등록</h1>
+                    <hr>
+                    <form action="/teacher" method="post">
+                        {{csrf_field()}}
+                        <div class="form-group">
+                            <div class="input-group">
+                                <div class="input-group-addon"><i class="fa fa-user"></i></div>
+                                <input type="text" name="name" class="form-control" placeholder="이름을 입력하세요">
+                            </div>
+                            <div class="input-group">
+                                <div class="input-group-addon"><i class="fa fa-user"></i></div>
+                                <input type="text" name="teacherPhone" class="form-control" placeholder="휴대폰번호를 입력하세요">
+                            </div>
+                            <div class="input-group">
+                                <div class="input-group-addon"><i class="fa fa-user"></i></div>
+                                <input type="text" name="address" class="form-control" placeholder="주소를 입력하세요">
+                            </div>
+                            <div class="input-group">
+                                <div class="input-group-addon"><i class="fa fa-user"></i></div>
+                                <input type="text" name="dob" class="form-control" placeholder="생년월일을 입력하세요">
+                            </div>
+                            <textarea name="memo" class="form-control" placeholder="특이사항을 입력하세요" rows="5"></textarea>
+                        </div>
+                        <button type="submit" class="btn new-btn">등록</button>
+                    </form>
+                    <br>
+                </div>
+            </div>
+        </div>
         <div class="row">
             <div class="col-lg-12">
                 <table id="teacher-table" class="table table-striped table-bordered" cellspacing="0">
